b6edc2513007418131709137d4d6751508cac3f2
mina-ima <h.imanaka@mdk-j.com>
2025-09-29 14:17:11 +0900

    feat: PWA設定とサービスワーカー登録、型定義の追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,52 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,wasm,onnx}"],
+      },
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+        "public/models/u2net.onnx",
+        "public/assets/fallback_bg/**/*",
+      ],
+      manifest: {
+        name: "Parallax Web App",
+        short_name: "ParallaxApp",
+        description:
+          "A client-side web application that generates 2.5D parallax effect videos.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
