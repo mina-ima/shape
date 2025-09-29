d968ed7291a315099106b5870db66c51fc2d9568
mina-ima <h.imanaka@mdk-j.com>
2025-09-29 14:48:31 +0900

    feat: PWAのSRI対応とinjectManifestモードへの切り替え

---
+++ src/sw.ts
@@ -0,0 +1,8 @@
+/// <reference lib="webworker" />
+// import * as precaching from "workbox-precaching";
+
+// declare const self: ServiceWorkerGlobalScope & {
+//   __WB_MANIFEST: (string | [string, string])[];
+// };
+
+// precaching.precacheAndRoute(self.__WB_MANIFEST);
